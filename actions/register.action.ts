a610d7cd657c92bd4f91b6ab740f07af8e9e2927
Prince Narteh <prinarteh@gmail.com>
2024-07-13 15:12:22 +0000

    feat: implements register

--- actions/register.action.ts
+++ actions/register.action.ts
@@ -1,16 +1,35 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import { RegisterSchema, RegisterSchemaType } from "@/schemas";
+import { db } from "@/lib/db";
+import constants from "@/constants";
 
 export const registerAction = async (data: RegisterSchemaType) => {
-  const validatedData = RegisterSchema.safeParse(data);
-  if (!validatedData.success) {
-    return {
-      error: "Invalid fields",
-    };
-  }
+  try {
+    const validatedData = RegisterSchema.safeParse(data);
+    if (!validatedData.success) {
+      return {
+        error: "Invalid fields",
+      };
+    }
 
-  return {
-    success: "Email sent!",
-  };
+    const { confirmPassword, ...res } = validatedData.data;
+    const hashedPassword = await bcrypt.hash(validatedData.data.password, 10);
+    await db.user.create({
+      data: {
+        ...res,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      success: "User created successfully.",
+    };
+  } catch (error: any) {
+    if (error.code === constants.DB_UNIQUE_ERROR_CODE) {
+      return { error: "Email already in used." };
+    }
+    return { error: "Internal server error" };
+  }
 };
---
+++ constants/index.ts
@@ -0,0 +1,3 @@
+export default {
+  DB_UNIQUE_ERROR_CODE: "P2002",
+};
