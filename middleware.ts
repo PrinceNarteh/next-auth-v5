9d265ab3cecefe089d55d4910b5cf5020edc68c5
Prince Narteh <prinarteh@gmail.com>
2024-07-14 21:44:17 +0000

    feat: start implementing middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { apiAuthPrefix, authRoutes, publicRoutes } from "./routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  // req.auth
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 });
 
 // Optionally, don't invoke Middleware on some paths
